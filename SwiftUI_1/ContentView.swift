c24de53975114dbea6bbf3e18af3208a68478fea
didierdelhaisse <did35@yahoo.com>
2020-05-26 16:55:26 +0200

    Initial Commit

---
+++ SwiftUI_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_1
+//
+//  Created by Didier Delhaisse on 26/05/2020.
+//  Copyright © 2020 Didier Delhaisse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
